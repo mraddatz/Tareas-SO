b218d10f72b315029d62e741c5e44e78a92f2e4c
mcorreaiz <mjcorrea5@uc.cl>
2018-03-30 12:17:04 -0300

    Listo el problema de los bursts. malloc hizo la magia

--- T1/mlfq.c
+++ T1/mlfq.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 struct Process {
     int PID;
     char nombre[256];
     char estado[20];
-    int bursts[4];
+    int *bursts;
 };
 
 typedef struct Process Proc;
@@ -16,35 +17,40 @@ void crear_proceso(char string[], int PID);
 int str2int(char ch);
 
 int main(int argc, char *argv[]){
-    char l[] = "proc_1 4 5 6 3";
+    char l[] = "proc_1 3 5 6 3";
     crear_proceso(l, 5);
     return 0;
 }
 
 void crear_proceso(char string[], int PID){
     char *ch;
-    int i;
-    i = 0;
-
-    Proc proceso;
+    int i = 0;
+    int N;
+    struct Process proceso;
     proceso.PID = PID;
-
-    proceso.bursts[0]=3;
-    proceso.bursts[3]=4;
 
     ch = strtok(string, " ");
     while (ch != NULL){
-        if (i == 0) strcpy(proceso.nombre, ch);
+        // Primer elemento es el nombre
+        if (i == 0) {
+            printf("debug\n\n\n");
+            strcpy(proceso.nombre, ch);
+        }
+        // Segundo elemento es N
+        else if (i == 1) {
+            N = str2int(*ch);
+            proceso.bursts = malloc(N * sizeof(int));
+        }
         else {
-            proceso.bursts[i-1] = str2int(*ch);
+            proceso.bursts[i-2] = str2int(*ch);
 
         };
         ch = strtok(NULL, " ");
         i++;
     }
 
-    printf("%s con bursts %d %d %d %d y PID %d", proceso.nombre, proceso.bursts[0], 
-    proceso.bursts[1], proceso.bursts[2], proceso.bursts[3], proceso.PID);
+    printf("%s con bursts %d %d %d y PID %d", proceso.nombre, proceso.bursts[0], 
+    proceso.bursts[1], proceso.bursts[2], proceso.PID);
 };
 
 int str2int(char ch) {
